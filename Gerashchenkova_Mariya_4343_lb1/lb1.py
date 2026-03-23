503b6bdbae485eb9ce1c83720eb666fffedb5210
Krouli-TUDUDU <mrgrsnkv@gmail.com>
2026-03-23 14:31:05 +0300

    Gerashchenkova_Mariya_lb1

--- Gerashchenkova_Mariya_4343_lb1/lb1.py
+++ Gerashchenkova_Mariya_4343_lb1/lb1.py
@@ -1,106 +1,131 @@
-def Solve(N):
-    h = [0] * N
-    best_tiles = []
-    tile = []
+def first_info(h, n):
+    min_h = h[0]
+    pos = 0
+    for i in range(1, n):
+        v = h[i]
+        if v < min_h:
+            min_h = v
+            pos = i
 
-    seen = {}
+    run = 1
+    i = pos + 1
+    while i < n and h[i] == min_h:
+        run += 1
+        i += 1
 
-    def pack(arr):
-        key = 0
-        base = N + 1
-        for v in arr:
-            key = key * base + v
-        return key
+    max_size = run
+    down = n - min_h
+    if max_size > down:
+        max_size = down
 
-    def Big_k(N):
-        hh = h[:]
-        tt = []
-        while True:
-            mh = hh[0]
-            x = 0
-            for i in range(1, N):
-                if hh[i] < mh:
-                    mh = hh[i]
-                    x = i
-            if mh == N:
-                break
-
-            y = mh
-
-            maxw = 0
-            while x + maxw < N and hh[x + maxw] == mh:
-                maxw += 1
-
-            k = min(maxw, N - y)
-            if x == 0 and y == 0 and k == N:
-                k = N - 1
-
-            for i in range(x, x + k):
-                hh[i] += k
-            tt.append((x + 1, y + 1, k))
-
-        return len(tt), tt
-
-    best_k, best_tiles = Big_k(N)
-
-    max_sq_area = (N - 1) * (N - 1)
-    total_area = N * N
-
-    def great(u, filled):
-        nonlocal best_k, best_tiles
-        if u >= best_k:
-            return
-
-        if u <= 10:
-            key = pack(h)
-            prev = seen.get(key)
-            if prev is not None and u >= prev:
-                return
-            seen[key] = u
-
-        if filled == total_area:
-            best_k = u
-            best_tiles = tile[:]
-            return
-
-        area_left = total_area - filled
-        lb = (area_left + max_sq_area - 1) // max_sq_area
-        if u + lb >= best_k:
-            return
-
-        mn = h[0]
-        x = 0
-        for i in range(1, N):
-            if h[i] < mn:
-                mn = h[i]
-                x = i
-        y = mn
-
-        maxw = 0
-        while x + maxw < N and h[x + maxw] == mn:
-            maxw += 1
-
-        max_size = min(maxw, N - y)
-        if x == 0 and y == 0 and max_size == N:
-            max_size = N - 1
-
-        for k in range(max_size, 0, -1):
-            for c in range(x, x + k):
-                h[c] += k
-            tile.append((x + 1, y + 1, k))
-
-            great(u + 1, filled + k * k)
-
-            tile.pop()
-            for c in range(x, x + k):
-                h[c] -= k
-
-    great(0, 0)
-
-    print(best_k)
-    for t in best_tiles:
-        print(*t)
+    return pos, min_h, max_size
 
 
-N = int(input())
-Solve(N)
+def greedy_bound(n):
+    h = [0] * n
+    placed = []
+
+    while True:
+        pos, min_h, max_size = first_info(h, n)
+        if min_h == n:
+            return placed
+
+        s = max_size
+        if not placed and s == n:
+            s -= 1
+
+        for j in range(pos, pos + s):
+            h[j] += s
+
+        placed.append((min_h, pos, s))
+
+
+def solve(n):
+    if n % 2 == 0:
+        s = n // 2
+        return [
+            (0, 0, s),
+            (0, s, s),
+            (s, 0, s),
+            (s, s, s)
+        ]
+
+    heights = [0] * n
+    current = []
+    best = greedy_bound(n)
+    best_count = len(best)
+    total = n * n
+    filled = 0
+    memo = {}
+
+    pos, h, max_size = first_info(heights, n)
+    if max_size == n:
+        max_size -= 1
+
+    stack = [[pos, h, max_size, n // 2 + 1, -1, -1, -1]]
+
+    while stack:
+        pos, h, next_size, min_size, pr, pc, ps = stack[-1]
+
+        if next_size < min_size:
+            stack.pop()
+            if ps != -1:
+                for j in range(pc, pc + ps):
+                    heights[j] -= ps
+                filled -= ps * ps
+                current.pop()
+            continue
+
+        s = next_size
+        stack[-1][2] = next_size - 1
+
+        for j in range(pos, pos + s):
+            heights[j] += s
+        filled += s * s
+        current.append((h, pos, s))
+        used = len(current)
+
+        prune = False
+
+        if filled == total:
+            if used < best_count:
+                best_count = used
+                best = current[:]
+            prune = True
+        elif used >= best_count:
+            prune = True
+        else:
+            _, min_h, _ = first_info(heights, n)
+            remain = total - filled
+            max_possible = n - min_h
+            low = (remain + max_possible * max_possible - 1) // (max_possible * max_possible)
+
+            if used + low >= best_count:
+                prune = True
+            else:
+                key = tuple(heights)
+                prev = memo.get(key)
+                if prev is not None and prev <= used:
+                    prune = True
+                else:
+                    memo[key] = used
+
+        if prune:
+            current.pop()
+            filled -= s * s
+            for j in range(pos, pos + s):
+                heights[j] -= s
+            continue
+
+        pos2, h2, max_size2 = first_info(heights, n)
+        stack.append([pos2, h2, max_size2, 1, h, pos, s])
+
+    return best
+
+
+n = int(input())
+ans = solve(n)
+
+print(len(ans))
+for r, c, s in ans:
+    print(r + 1, c + 1, s)
